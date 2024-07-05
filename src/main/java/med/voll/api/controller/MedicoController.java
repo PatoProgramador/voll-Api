24fd7e9cd075f2f46cd8edf451d0cd50ebcd5902
PatoProgramador <skpates18@gmail.com>
2024-07-05 18:13:44 -0500

    Medico DTO

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,14 +1,15 @@
 package med.voll.api.controller;
 
 import jakarta.validation.Valid;
+import med.voll.api.model.medico.DatosListadoMedico;
 import med.voll.api.model.medico.DatosRegistroMedico;
 import med.voll.api.model.medico.Medico;
 import med.voll.api.model.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/medicos")
@@ -16,6 +17,13 @@ public class MedicoController {
 
     @Autowired
     private MedicoRepository medicoRepository;
+
+    @GetMapping
+    public List<DatosListadoMedico> listadoMedicos() {
+        return medicoRepository.findAll().stream()
+                .map(DatosListadoMedico::new)
+                .collect(Collectors.toList());
+    }
 
     @PostMapping
     public Medico registrarMedico(@RequestBody @Valid DatosRegistroMedico datosRegistroMedico) {
