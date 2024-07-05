1b00263eb175d9584d3339979a0a75ee13c00bbd
PatoProgramador <skpates18@gmail.com>
2024-07-05 15:17:42 -0500

    Cors configuration

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,6 +1,7 @@
 package med.voll.api.controller;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MedicoController {
 
     @PostMapping
-    public void registrarMedico() {
-        System.out.println("Creao :)");
+    public void registrarMedico(@RequestBody String payload) {
+        System.out.println("Creao :)" + payload);
     }
 }
