cbd2304b438919efa8737f2e5b657ab78a8b1f12
PatoProgramador <skpates18@gmail.com>
2024-07-08 16:19:52 -0500

    Add Spring security dependencies

--- src/main/java/med/voll/api/infra/ErrorsHandler.java
+++ src/main/java/med/voll/api/infra/ErrorsHandler.java
@@ -3,7 +3,6 @@ package med.voll.api.infra;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
