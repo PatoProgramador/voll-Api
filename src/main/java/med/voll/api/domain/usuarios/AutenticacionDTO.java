42fd671ab3e8b427cd36713e2747561f221777ed
PatoProgramador <skpates18@gmail.com>
2024-07-08 17:32:04 -0500

    Add java jwt

--- src/main/java/med/voll/api/domain/usuarios/AutenticacionDTO.java
+++ src/main/java/med/voll/api/domain/usuarios/AutenticacionDTO.java
@@ -1,7 +1,11 @@
 package med.voll.api.domain.usuarios;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record AutenticacionDTO(
+        @NotBlank
         String login,
+        @NotBlank
         String clave
 ) {
 }
