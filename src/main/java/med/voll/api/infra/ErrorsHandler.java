dad9eeffd53a1b85fd53e81d55c2c6d002747bb6
PatoProgramador <skpates18@gmail.com>
2024-07-08 15:49:58 -0500

    Error handler to post fields

--- src/main/java/med/voll/api/infra/ErrorsHandler.java
+++ src/main/java/med/voll/api/infra/ErrorsHandler.java
@@ -2,14 +2,33 @@ package med.voll.api.infra;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
 
 @RestControllerAdvice
 public class ErrorsHandler {
 
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity errorHandler404() {
+    public ResponseEntity errorHandle404() {
        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity errorHandle400(MethodArgumentNotValidException e) {
+        List<ErrorValidationDTO> errors = e.getFieldErrors().stream()
+                .map(ErrorValidationDTO::new)
+                .toList();
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+    private record ErrorValidationDTO(String field, String error) {
+        public ErrorValidationDTO(FieldError error) {
+            this(error.getField(), error.getDefaultMessage());
+        }
     }
 }
